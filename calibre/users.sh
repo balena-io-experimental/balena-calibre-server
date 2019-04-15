84a74043b9ebcc0cf860f55de95487e85a1b3d33
Gergely Imreh <gergely@balena.io>
2019-04-15 13:28:04 +0100

    Update user updater script to properly kill calibre when needed
    Change-type: patch
    Signed-off-by: Gergely Imreh <gergely@balena.io>

--- calibre/users.sh
+++ calibre/users.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo "🛑 Stopping running Calibre server to adjust users"
-killall $(cat /var/run/calibre.pid) 2> /dev/null || true
+CALIBRE_PID=$(cat /var/run/calibre.pid)
+if [ -n "$CALIBRE_PID" ]; then
+    kill "$CALIBRE_PID" 2>/dev/null || true
+    timeout 10 tail --pid="$CALIBRE_PID" -f /dev/null
+fi
 
 calibre-server --manage-users --userdb=/calibre_data/users.sqlite
 
